7606d0b0c842021a26e8a49140fee14c15453893
Karina Pliego <karinapliegovergara@gmail.com>
2023-06-01 22:41:24 -0500

    Completed Array Find Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+// If there was a win that occured, will return the year. If not, returns undefined
+  function superbowlWin(record) {
+    let outcome = record.find(record => record.result === 'W');
+    if (outcome) {
+        return outcome.year;
+    } else {
+        return undefined;
+    }
+}
